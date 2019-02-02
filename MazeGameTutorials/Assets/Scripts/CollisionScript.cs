72fbfdeb93ba61fea7e58cddb5be4841509b77d9
Jonathan Ferguson <jferguson@champlain.edu>
2019-02-02 02:19:28 -0500

    initial commit of MazeGameTutorials
    This project matches with the following Tutorial sequences:
    
    * Maze Game

---
+++ MazeGameTutorials/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+   
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionEnter2D");
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionExit2D");
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionStay2D");
+    }
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MazeGameTutorials/Assets/Scripts/RandomCollectible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        RepositionCollectible();
+    }
+    void RepositionCollectible()
+    {
+        float newX = Random.Range (-5, 5);
+        float newY = Random.Range (-3, 3);
+        Vector2 newPos = new Vector2 (newX, newY);
+        transform.position = newPos;
+        GameObject.Find ("Score_Canvas").GetComponent<ScoreScript> ().AddScore ();
+    }
+}
