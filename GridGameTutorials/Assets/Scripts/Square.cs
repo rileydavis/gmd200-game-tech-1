df3727394ddbecf1a035efee01e9c03a5f3ba15f
Jonathan Ferguson <jferguson@champlain.edu>
2019-02-07 01:40:53 -0500

    updating BasicGrid with minor cleanups, and fixes to scenes

--- GridGameTutorials/Assets/Scripts/Square.cs
+++ GridGameTutorials/Assets/Scripts/Square.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Square : MonoBehaviour
 {
     public Vector2Int gridPosition;
-    public Material mouseOverMaterial; //Assign in Inspector: drag MouseOverMaterial into Square prefab
+    public Material mouseOverMaterial; //Drag MouseOverMaterial into Square prefab's Square script to assign.
     
     //When the mouse hovers over the GameObject, it turns to this color
     static Color mouseOverColor;
@@ -14,34 +14,30 @@ public class Square : MonoBehaviour
     Color originalColor;
 
     //Get the GameObject’s spriteRenderer to access the GameObject’s material and color
-    SpriteRenderer spriteRenderer;
+    SpriteRenderer spriteRenderer; //A spriteRenderer to get and swap colors
 
     void Awake()
     {
-        //Fetch the spriteRenderer component from the GameObject
+        //Get the spriteRenderer component from the GameObject
         spriteRenderer = GetComponent<SpriteRenderer>();
-        //Fetch the original color of the GameObject
+        //Get the original color of the GameObject
         originalColor = spriteRenderer.material.color;
         mouseOverColor = mouseOverMaterial.color;
+
         //mouseOverColor = Resources.Load<Material>("Materials/MouseOverColor").color;
         // avoid using Resources if you want to build the project see:
         // https://unity3d.com/learn/tutorials/topics/best-practices/resources-folder
-
     }
     void Start()
     {
 
     }
+    //OnMouseOver() tells Unity what should happen when a mouse is over the object
     void OnMouseOver()
     {
         Debug.Log(name); //unity implicitly defines "name" for you to get the "name" of the object.
         GridManager.UpdateUI(this); //the keyword "this" refers to the current object, aka "this" object.
-        spriteRenderer.material.color = mouseOverColor;
-        //change color while mouse is over the object
-
-        // notice that "gameObject" is a convenience
-        // gameObject will always return the object that this component (script)
-        // is attached to. 
+        spriteRenderer.material.color = mouseOverColor; //change color while mouse is over the object
     }
 
     void OnMouseExit()
