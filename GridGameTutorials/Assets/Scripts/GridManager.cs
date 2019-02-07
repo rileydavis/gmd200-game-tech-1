ad79cca55cc6ee5578f453488a43ac11134d0d37
Jonathan Ferguson <jferguson@champlain.edu>
2019-02-06 19:22:34 -0500

    substantially refactored the code for GridGameTutorials
    * Removed GridInfoScript entirely.
    
    * Removed deprecated Resources.Load calls, see: https://unity3d.com/learn/tutorials/topics/best-practices/resources-folder
    * Made GridManager a proper manager class (private singleton)
    ** Allowing calls that appear like this:
    *** GridManager.UpdateUI(this);
    ** Used Square data members in BuildGrid()
    
    * Refactored Square to incorporate:
    ** Data members for position and color
    ** Enabled custom Material to be applied without calls to custom OnOver()
    
    * General edits fol logical clarity

--- GridGameTutorials/Assets/Scripts/GridManager.cs
+++ GridGameTutorials/Assets/Scripts/GridManager.cs
@@ -1,30 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GridManager : MonoBehaviour
 {
     [HideInInspector]
     public GameObject gridHolder;
-    GameObject squarePrefab;
+    public Square squarePrefab; //assign in Inspector on Grid_Manager to Assets/Prefabs/Square
     public float startX = 0;
     public float startY = 0;
     public int rows;
     public int cols;
     float spacer = 0.1f;
 
+    // how to set this up without a public singleton.
+    // this will appear to be a public static class.
+    public Text nameTF;
+    public Text rowTF;
+    public Text colTF;
+
+    private static GridManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        squarePrefab = Resources.Load<GameObject>("Prefabs/Square");
+        //squarePrefab = Resources.Load<Square>("Prefabs/Square");
+        // avoid using Resources if you want to build the project see:
+        // https://unity3d.com/learn/tutorials/topics/best-practices/resources-folder
+
         InitGridHolder();
         BuildGrid();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        instance = this;
     }
 
     void InitGridHolder()
@@ -40,11 +48,31 @@ public class GridManager : MonoBehaviour
         {
             for (int j = 0; j < cols; j++)
             {
-                GameObject square = Instantiate(squarePrefab, gridHolder.transform);
+                Square square = Instantiate<Square>(squarePrefab, gridHolder.transform);
                 Vector2 newPos = new Vector2(j + (spacer * j), i + (spacer * i));
                 square.transform.localPosition = newPos;
                 square.name = "Square_" + i + "_" + j;
+                square.gridPosition = new Vector2Int(i, j);
             }
         }
+    }
+
+    // note that wiring these up in the Inspector can sometimes be fragile:
+    // if you change the variable type or set them to static, then
+    // you may need to rewire these.
+
+    public static void UpdateUI(Square square = null)
+    {
+        if (square == null)
+        {
+            instance.nameTF.text = "---";
+            instance.rowTF.text = "---";
+            instance.colTF.text = "---";
+            return;
+        }
+
+        instance.nameTF.text = square.name;
+        instance.colTF.text = square.gridPosition.x.ToString();
+        instance.rowTF.text = square.gridPosition.y.ToString();
     }
 }
---
+++ GridGameTutorials/Assets/Scripts/Square.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    public Vector2Int gridPosition;
+    public Material mouseOverMaterial; //Assign in Inspector: drag MouseOverMaterial into Square prefab
+    
+    //When the mouse hovers over the GameObject, it turns to this color
+    static Color mouseOverColor;
+
+    //This stores the GameObject’s original color
+    Color originalColor;
+
+    //Get the GameObject’s spriteRenderer to access the GameObject’s material and color
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        //Fetch the spriteRenderer component from the GameObject
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //Fetch the original color of the GameObject
+        originalColor = spriteRenderer.material.color;
+        mouseOverColor = mouseOverMaterial.color;
+        //mouseOverColor = Resources.Load<Material>("Materials/MouseOverColor").color;
+        // avoid using Resources if you want to build the project see:
+        // https://unity3d.com/learn/tutorials/topics/best-practices/resources-folder
+
+    }
+    void Start()
+    {
+
+    }
+    void OnMouseOver()
+    {
+        Debug.Log(name); //unity implicitly defines "name" for you to get the "name" of the object.
+        GridManager.UpdateUI(this); //the keyword "this" refers to the current object, aka "this" object.
+        spriteRenderer.material.color = mouseOverColor;
+        //change color while mouse is over the object
+
+        // notice that "gameObject" is a convenience
+        // gameObject will always return the object that this component (script)
+        // is attached to. 
+    }
+
+    void OnMouseExit()
+    {
+        GridManager.UpdateUI();
+        spriteRenderer.material.color = originalColor;
+    }
+}
