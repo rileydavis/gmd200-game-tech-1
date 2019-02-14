b00e0422eb12f49f21ff1c12e4fe5f9a4c6b72a6
Jonathan Ferguson <jferguson@champlain.edu>
2019-02-14 11:57:50 -0500

    adding work-in-progress on Hearts and Health
    * Created a public static class: Health
    * Started refactoring Heart for new approach to Health
    ** By storing Health data in a static class, anything can alter those data members (variables)
    ** While this is convenient, maybe there are better ways?

---
+++ VerticalShooterTutorials/Assets/Scripts/Health.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Health
+{
+    public static int currentHealth = 4;//the current health of the heart
+    public static int maxHealth = 4;//the max health of the heart
+
+}
--- VerticalShooterTutorials/Assets/Scripts/Heart.cs
+++ VerticalShooterTutorials/Assets/Scripts/Heart.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 public class Heart : MonoBehaviour
 {
     public Sprite[] sprites; //assignment of this array is done in the Unity Inspector
-    public int currentHealth = 4;//the current health of the heart
-    public int maxHealth = 4;//the max health of the heart
     public float crossFadeToAlpha = 0f; //CrossFadeAlpha value from 0..1
     public float crossFadeToOpaque = 1f; //CrossFadeAlpha value from 0..1
     public float crossFadeTime = 3f; //CrossFade time in seconds
@@ -24,27 +22,27 @@ public class Heart : MonoBehaviour
     public void Damage() //A quick function to cause damage...
     {
 
-        if (currentHealth > 0)
+        if (Health.currentHealth > 0)
         {
-            currentHealth--;
+            Health.currentHealth--;
             SetSprite();
         }
-        if (currentHealth == 0)
+        if (Health.currentHealth == 0)
         {
-            image.sprite = sprites[currentHealth];
+            image.sprite = sprites[Health.currentHealth];
             image.CrossFadeAlpha(crossFadeToAlpha, crossFadeTime, isIgnoreTime);
         }
     }
 
     void SetSprite() //Set Sprite will swap the image based on the currentHealth:
     {
-        image.sprite = sprites[currentHealth];
+        image.sprite = sprites[Health.currentHealth];
     }
 
     [ContextMenu("Reset Health")]
     public void Reset()
     {
-        currentHealth = maxHealth;
+        Health.currentHealth = Health.maxHealth;
         SetSprite();
         image.CrossFadeAlpha(crossFadeToOpaque, crossFadeTime, isIgnoreTime);
     }
